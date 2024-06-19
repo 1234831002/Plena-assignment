d4dea6e0b54ffef9c5285635e1a2aec1d61840c6
Abhijeet-dev94 <abhijeetsahu30ak@gmail.com>
2024-06-19 21:40:25 +0530

    feat:search with caching

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,10 +1,34 @@
-import { createClient } from 'redis';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { createClient, RedisClientType } from 'redis';
 
-const client = createClient({
-    password: 'DcFelyWVQf1dxKuZBWLhfyH2NZ1Hr4uC',
-    socket: {
-        host: 'redis-13969.c325.us-east-1-4.ec2.redns.redis-cloud.com',
-        port: 13969
+@Injectable()
+export class RedisService implements OnModuleInit, OnModuleDestroy {
+    private client: RedisClientType;
+
+    async onModuleInit() {
+        this.client = createClient({
+            password: 'DcFelyWVQf1dxKuZBWLhfyH2NZ1Hr4uC',
+            socket: {
+                host: 'redis-13969.c325.us-east-1-4.ec2.redns.redis-cloud.com',
+                port: 13969
+            }
+        });
+        await this.client.connect();
     }
-});
-export default client
+
+    async onModuleDestroy() {
+        await this.client.disconnect();
+    }
+
+    async get(key: string): Promise<string | null> {
+        return await this.client.get(key);
+    }
+
+    async set(key: string, value: string, ttl: number): Promise<void> {
+        await this.client.set(key, value, { EX: ttl });
+    }
+
+    async del(key: string): Promise<void> {
+        await this.client.del(key);
+    }
+}
